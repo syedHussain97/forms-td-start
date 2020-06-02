e67358b69129eff9eb040607ec91c3d95f5bbbbc
Syed Hussain <syed.m.husyn@gmail.com>
2020-06-03 08:55:56 +1200

    adding directives to reference controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,19 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultQuestion = 'pet';
+  @ViewChild('f', {static: false})
+  form: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.form);
   }
 }
